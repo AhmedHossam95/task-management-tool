997f15214c86b8f5f9161a293263600e87cd4137
AhmedHossam95 <ahmed_hossam_eldeen@outlook.com>
2026-01-31 20:44:02 +0200

    task: added custom date adapter for mat datepicker format DD-MM-YYYY

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,17 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { routes } from './app.routes';
+import { CustomDateAdapter, DD_MM_YYYY_FORMAT } from './shared/utils/custom-date-adapter';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    { provide: DateAdapter, useClass: CustomDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: DD_MM_YYYY_FORMAT },
   ],
 };
